cac7c4a821b3b06504c3ec2123f76a80bf97a327
Kevin Cuello <ckuello@gmail.com>
2019-03-02 20:26:29 +0000

    database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,10 +5,10 @@ class TodoController < ApplicationController
     def show
         todo_id = params[:id]
         if todo_id =='1'
-            @todo_description ="Make Dinner"
+            @todo_description ="Go to post office"
             @todo_pomodoro_estimate = 4
         elsif todo_id =='2'
-            @todo_description = "DO homework"
+            @todo_description = "Cook food"
             @todo_pomodoro_estimate = 5
             
         end
