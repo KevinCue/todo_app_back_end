a97d7de583f6aa7746fd6f31d4ef31333bd63fc3
Kevin Cuello <ckuello@gmail.com>
2019-03-02 18:50:28 +0000

    add variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,9 @@
 class TodoController < ApplicationController
     def index
     end
+    
+    def show
+        @todo_description = "Make Dinner"
+        @todo_pomodoro_estimate = 17
+    end
 end
