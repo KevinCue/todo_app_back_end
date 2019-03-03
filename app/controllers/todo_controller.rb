f93d53db6fd9f5eb4619847bbc3534a98360721e
Kevin Cuello <ckuello@gmail.com>
2019-03-03 19:56:41 +0000

    Format fix

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -28,7 +28,7 @@ class TodoController < ApplicationController
         t.description = params['description']
         t.pomodoro_estimate = params['pomodoro_estimate']
         t.save
-    redirect_to "/todo/show/#{t.id}"    
+        redirect_to "/todo/show/#{t.id}"
     end
     
     def destroy
