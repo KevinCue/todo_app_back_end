40a50aa273e31fafc9834d0cdf3d4d9104f8c034
Kevin Cuello <ckuello@gmail.com>
2019-03-03 19:29:35 +0000

    delete and edit

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -12,11 +12,28 @@ class TodoController < ApplicationController
     end
     
     def create
-    t = Todo.new
-    t.description = params['description']
-    t.pomodoro_estimate = params['pomodoro_estimate']
-    t.save
-    redirect_to "/todo/show/#{t.id}"
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
     end
-
+    
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    
+    def update
+        t = Todo.find_by_id(params[:id])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+    redirect_to "/todo/show/#{t.id}"    
+    end
+    
+    def destroy
+    t = Todo.find_by_id(params[:id])
+    t.destroy
+    redirect_to '/todo/index'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
  get 'todo/show/:id' , to: 'todo#show'
  get 'todo/new' , to: 'todo#new'
  post 'todo/new' , to: 'todo#create'
+ get 'todo/edit/:id' , to: 'todo#edit'
+ post 'todo/edit/:id', to: 'todo#update'
+ get 'todo/destroy/:id' , to: 'todo#destroy'
 end
